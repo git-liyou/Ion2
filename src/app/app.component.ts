b4c2cefce9bd22d1fd93cdc38ab97fc7f0202416
Li Just <Li Just>
2016-12-02 15:39:58 +0800

    Menu页面调整

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,6 +39,6 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    this.nav.push(page.component);
   }
 }

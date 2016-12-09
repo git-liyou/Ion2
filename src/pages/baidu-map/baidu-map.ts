574cf765a69883d32501c31f6a1e467f3f6a2513
Li Just <100602650@qq.com>
2016-12-09 10:55:15 +0800

    百度地图定位失败处理

--- src/pages/baidu-map/baidu-map.ts
+++ src/pages/baidu-map/baidu-map.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, MenuController } from 'ionic-angular';
+import { NavController, MenuController，ToastController } from 'ionic-angular';
 
 declare var BMap, baidu_location;
 @Component({
@@ -8,15 +8,21 @@ declare var BMap, baidu_location;
 })
 export class BaiduMapPage {
   map: any;
-  constructor(public navCtrl: NavController, public menuCtrl: MenuController) {
+  constructor(public navCtrl: NavController, public menuCtrl: MenuController, public toastCtrl: ToastController) {
     this.menuCtrl.swipeEnable(false);
   }
   getLocation() {
+    let that = this;
     baidu_location.getCurrentPosition((data) => {
       alert(JSON.stringify(data));
-      this.map.centerAndZoom(new BMap.Point(data.lontitude, data.latitude), 15);
+      if (data.locType == '61' || data.locType == '161') {
+        this.map.centerAndZoom(new BMap.Point(data.lontitude, data.latitude), 15);
+      } else {
+        that.showToast('定位失败，请检查应用权限设置！');
+      }
+
     }, (err) => {
-      alert("错误：" + JSON.stringify(err))
+      that.showToast('定位失败，请检查应用权限设置！');
     });
   }
   ionViewDidLoad() {
@@ -24,9 +30,15 @@ export class BaiduMapPage {
     this.map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
     this.map.addControl(new BMap.NavigationControl());
     this.map.enableScrollWheelZoom(true);
-
   }
   ionViewWillLeave() {
     this.menuCtrl.swipeEnable(true);
+  }
+  showToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
   }
 }

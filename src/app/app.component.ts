10684e40a351f94dc0a84bb76dd43f77947321df
Li Just <100602650@qq.com>
2016-12-14 17:16:06 +0800

    增加android返回键监听；minSdkVersion提升

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, ToastController, MenuController } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 import { TabsPage } from '../pages/tabs/tabs';
 import { BaiduMapPage } from '../pages/baidu-map/baidu-map';
@@ -11,12 +11,11 @@ import { EchartsPage } from '../pages/echarts/echarts';
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
-
   rootPage: any = TabsPage;
-
   pages: Array<{ title: string, icon: string, component: any }>;
+  backButtonPressed: boolean = false;
 
-  constructor(public platform: Platform) {
+  constructor(public platform: Platform, public toastCtrl: ToastController, public menuCtrl: MenuController) {
     this.initializeApp();
 
     this.pages = [
@@ -30,9 +29,48 @@ export class MyApp {
     this.platform.ready().then(() => {
       StatusBar.styleDefault();
       Splashscreen.hide();
+      this.platform.registerBackButtonAction((): any => {
+        let activeVC = this.nav.getActive();
+        let page = activeVC.instance;
+
+        if (this.menuCtrl.isOpen()) return this.menuCtrl.close();
+
+        if (!(page instanceof TabsPage)) {
+          if (!this.nav.canGoBack()) {
+            //当前页面为tabs，退出APP
+            return this.showExit();
+          }
+          //当前页面为tabs的子页面，正常返回
+          return this.nav.pop();
+        }
+
+        let tabs = page.tabs;
+        let activeNav = tabs.getSelected();
+
+        if (!activeNav.canGoBack()) {
+          //当前页面为tab栏，退出APP
+          return this.showExit();
+        }
+        //当前页面为tab栏的子页面，正常返回
+        return activeNav.pop();
+      }, 101);
     });
   }
-
+  showExit() {
+    if (this.backButtonPressed) this.platform.exitApp();
+    else {
+      let toast = this.toastCtrl.create({
+        message: '再按一次退出应用',
+        duration: 2000,
+        position: 'bottom'
+      });
+      toast.present();
+      this.backButtonPressed = true;
+      setTimeout(() => {
+        this.backButtonPressed = false;
+      }, 2000)
+    }
+  }
   openPage(page) {
     this.nav.push(page.component);
   }
